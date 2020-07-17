e60082914cc9de9ec67f0abb6924ddce1de77058
joshNic <256jomu@gmail.com>
2020-07-17 12:58:48 +0300

    add room db setup

---
+++ app/src/main/java/com/example/qkc/data/db/UserDao.kt
@@ -0,0 +1,20 @@
+package com.example.qkc.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.qkc.data.db.entity.CURRENT_USER_ID
+import com.example.qkc.data.db.entity.User
+
+@Dao
+interface UserDao{
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user: User) : Long
+
+    @Query("SELECT * FROM user WHERE uid = $CURRENT_USER_ID")
+    fun getuser() : LiveData<User>
+
+}
