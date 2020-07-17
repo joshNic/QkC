716c0665ea3234d26bb17acf856520b2dc5f3b51
joshNic <256jomu@gmail.com>
2020-07-17 17:18:06 +0300

    add dependency injection using kodein

---
+++ app/src/main/java/com/example/qkc/QkCApplication.kt
@@ -0,0 +1,30 @@
+package com.example.qkc
+
+import android.app.Application
+import com.example.qkc.data.db.AppDatabase
+import com.example.qkc.data.network.ApiService
+import com.example.qkc.data.network.NetworkConnectionInterceptor
+import com.example.qkc.data.repository.UserRepository
+import com.example.qkc.ui.auth.AuthViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class QkCApplication: Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@QkCApplication))
+
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
+        bind() from singleton { ApiService(instance()) }
+        bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { UserRepository(instance(), instance()) }
+        bind() from provider { AuthViewModelFactory(instance()) }
+
+    }
+
+}
--- app/src/main/java/com/example/qkc/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/qkc/ui/auth/LoginActivity.kt
@@ -7,37 +7,33 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.qkc.R
-import com.example.qkc.data.db.AppDatabase
 import com.example.qkc.data.db.entity.User
-import com.example.qkc.data.network.ApiService
-import com.example.qkc.data.network.NetworkConnectionInterceptor
-import com.example.qkc.data.repository.UserRepository
 import com.example.qkc.databinding.ActivityLoginBinding
 import com.example.qkc.ui.home.HomeActivity
 import com.example.qkc.ui.util.hide
 import com.example.qkc.ui.util.show
 import com.example.qkc.ui.util.snackbar
 import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.closestKodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity(), AuthListener {
+class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
+    override val kodein by closestKodein()
+    private val factory: AuthViewModelFactory by instance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
-        val api = ApiService(networkConnectionInterceptor)
-        val db = AppDatabase(this)
-        val repository = UserRepository(api, db)
-        val factory = AuthViewModelFactory(repository)
-
         val binding: ActivityLoginBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_login)
-        val viewModel = ViewModelProvider(this,factory).get(AuthViewModel::class.java)
+        val viewModel = ViewModelProvider(this, factory).get(AuthViewModel::class.java)
         binding.viewmodel = viewModel
 
         viewModel.authListener = this
 
         viewModel.getLoggedInUser().observe(this, Observer { user ->
-            if(user != null){
+            if (user != null) {
                 Intent(this, HomeActivity::class.java).also {
                     it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(it)
