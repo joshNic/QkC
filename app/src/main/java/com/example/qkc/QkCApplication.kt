01159ba6a2f8bfe8ff413574af5c943a22da7c59
joshNic <256jomu@gmail.com>
2020-07-18 01:55:57 +0300

    add caching strategy for quotes

--- app/src/main/java/com/example/qkc/QkCApplication.kt
+++ app/src/main/java/com/example/qkc/QkCApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.example.qkc.data.db.AppDatabase
 import com.example.qkc.data.network.ApiService
 import com.example.qkc.data.network.NetworkConnectionInterceptor
+import com.example.qkc.data.preferences.PreferenceProvider
 import com.example.qkc.data.repository.QuotesRepository
 import com.example.qkc.data.repository.UserRepository
 import com.example.qkc.ui.auth.AuthViewModelFactory
@@ -17,7 +18,8 @@ import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
-class QkCApplication: Application(), KodeinAware {
+class QkCApplication : Application(), KodeinAware {
+
 
     override val kodein = Kodein.lazy {
         import(androidXModule(this@QkCApplication))
@@ -25,11 +27,12 @@ class QkCApplication: Application(), KodeinAware {
         bind() from singleton { NetworkConnectionInterceptor(instance()) }
         bind() from singleton { ApiService(instance()) }
         bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { PreferenceProvider(instance()) }
         bind() from singleton { UserRepository(instance(), instance()) }
-        bind() from singleton { QuotesRepository(instance(), instance()) }
+        bind() from singleton { QuotesRepository(instance(), instance(), instance()) }
         bind() from provider { AuthViewModelFactory(instance()) }
         bind() from provider { ProfileViewModelFactory(instance()) }
-        bind() from provider{ QuotesViewModelFactory(instance()) }
+        bind() from provider { QuotesViewModelFactory(instance()) }
 
     }
 
--- app/src/main/java/com/example/qkc/data/repository/QuotesRepository.kt
+++ app/src/main/java/com/example/qkc/data/repository/QuotesRepository.kt
@@ -6,13 +6,20 @@ import com.example.qkc.data.db.AppDatabase
 import com.example.qkc.data.db.entity.Quote
 import com.example.qkc.data.network.ApiService
 import com.example.qkc.data.network.SafeApiRequest
+import com.example.qkc.data.preferences.PreferenceProvider
 import com.example.qkc.ui.util.Coroutines
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+private val MINIMUM_INTERVAL = 6
+
 
 class QuotesRepository(
     private val api: ApiService,
-    private val db: AppDatabase
+    private val db: AppDatabase,
+    private val prefs: PreferenceProvider
 ) : SafeApiRequest() {
 
     private val quotes = MutableLiveData<List<Quote>>()
@@ -31,19 +38,25 @@ class QuotesRepository(
     }
 
     private suspend fun fetchQuotes() {
-        if (isFetchNeeded()) {
+        val lastSavedAt = prefs.getLastSavedAt()
+
+        if (lastSavedAt == null || isFetchNeeded(LocalDateTime.parse(lastSavedAt))) {
             val response = apiRequest { api.getQuotes() }
             quotes.postValue(response.quotes)
         }
     }
 
-    private fun isFetchNeeded(): Boolean {
-        return true
+
+    private fun isFetchNeeded(savedAt: LocalDateTime): Boolean {
+        return ChronoUnit.HOURS.between(savedAt, LocalDateTime.now()) > MINIMUM_INTERVAL
     }
+
+
 
 
     private fun saveQuotes(quotes: List<Quote>) {
         Coroutines.io {
+            prefs.savelastSavedAt(LocalDateTime.now().toString())
             db.getQuoteDao().saveAllQuotes(quotes)
         }
     }
